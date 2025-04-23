acdfa74c73bc6e19fbc540951780d572b487fbf3
santiago salas <santiagoagustin004@gmail.com>
2025-04-23 04:49:37 -0500

    Create 01.py

---
+++ 01.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+def sliding_window_max(nums, k):
+    dq = deque()
+    result = []
+    for i, n in enumerate(nums):
+        while dq and nums[dq[-1]] < n:
+            dq.pop()
+        dq.append(i)
+        if dq[0] == i - k:
+            dq.popleft()
+        if i >= k - 1:
+            result.append(nums[dq[0]])
+    return result
